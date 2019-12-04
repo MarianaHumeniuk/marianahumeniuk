8cda8125189ed0a5bdb36b535c8c629d1b77145e
MarianaHumeniuk <mariana_humeniuk@yahoo.com>
2019-12-04 16:35:10 +0200

    https

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1576067130;s:7:"authKey";s:64:"u+o?=f#qPQh_e%bG s=jly9U4.y?jy4T>mf0If#b[DH>N8OUIDjr~S.g};Nx#yQ0";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:2116;}
